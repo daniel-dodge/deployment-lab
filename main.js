c9105c03cbd5ab899472aa1be1cf24d2bee8076b
Daniel Dodge <dandantheman87@gmail.com>
2021-12-15 14:31:17 -0700

    add css js files

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("workrkowrkwo")
+alert("workrkowrkwo")
--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/main.js'))
+    res.sendFile(path.join(__dirname, '../main.js'))
   })
-app.use('/css', express.static(path.join(__dirname, 'public/styles.css')))
+app.use('/css', express.static(path.join(__dirname, '../styles.css')))
 const port = process.env.PORT || 4005
 app.listen(port, () => {console.log(`port ${port} is up`)})
