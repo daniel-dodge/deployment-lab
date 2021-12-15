2321079b7eff5ac6e5048ec6e217ee9edbaac491
Daniel Dodge <dandantheman87@gmail.com>
2021-12-15 14:26:08 -0700

    attempt 4

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const app = express()
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
-app.use('/js', express.static(path.join(__dirname, '/public/main.js')))
-app.use('/css', express.static(path.join(__dirname, '/public/styles.css')))
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/main.js'))
+  })
+app.use('/css', express.static(path.join(__dirname, 'public/styles.css')))
 const port = process.env.PORT || 4005
 app.listen(port, () => {console.log(`port ${port} is up`)})
