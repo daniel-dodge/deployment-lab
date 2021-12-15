1d7b7caacc0bb347f762f3e5d070f14f4c67b223
Daniel Dodge <dandantheman87@gmail.com>
2021-12-15 14:06:49 -0700

    attempted js and css implementation

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const app = express()
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
-
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+app.use('/css', express.static(path.join(__dirname, 'public/styles.css')))
 const port = process.env.PORT || 4005
 app.listen(port, () => {console.log(`port ${port} is up`)})
