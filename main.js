b3a9ebc8605d9e0bbbe8f95769c85b75a7c64bfa
Daniel Dodge <dandantheman87@gmail.com>
2021-12-15 17:08:18 -0700

    finished css and javascript

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert("workrkowrkwo")
+// alert("workrkowrkwo")
+const button = document.querySelector('#purchase-btn')
+const chairContainer = document.querySelector('#yard-box')
+
+const addChair = () => {
+    let newChair = document.createElement('div')
+    newChair.classList.add('newImage')
+    chairContainer.appendChild(newChair)
+}
+
+button.addEventListener('click', addChair)
